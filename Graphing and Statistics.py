eefc3e92cdbdbcfa8a9a3d52658513cb191e2a27
Jake Holt <37301092+JakeH15@users.noreply.github.com>
2024-10-09 09:12:49 -0400

    Add files via upload
    Swapped figures in 2A and SB, added transform as a function

--- Graphing and Statistics.py
+++ Graphing and Statistics.py
@@ -10,9 +10,9 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 from scipy.stats import mannwhitneyu
+
 """
 https://stackoverflow.com/questions/36153410/how-to-create-a-swarm-plot-with-matplotlib
-
 """
 def simple_beeswarm2(y, nbins=10, width=.25):
     """
@@ -59,6 +59,23 @@ def simple_beeswarm2(y, nbins=10, width=.25):
             x[b] = (0.5 + j / 3 + np.arange(len(b))) * -dx
 
     return x
+
+"""
+for log plot
+transform the data (0 becomes 10**-5)
+maximum biovolume of reporter strain (for both pilU and WT) at 4 d is ~10^5, -> limit of detection is set to 10^-5
+to put the data on a log scale 0 values for the GFP channel, the transformaiton reporter channel, are set to 1x10^-5
+"""
+def transform(dict_in):
+    for key in list(dict_in.keys()):
+        holder = []
+        for i in dict_in[key]:
+            if i == 0:
+                holder.append(10**-5)
+            else:
+                holder.append(i)
+        dict_in[key] = holder
+    return dict_in
 #%%
 """
 Figure 1C
@@ -68,6 +85,9 @@ df = pd.read_excel('SI_Data.xlsx', sheet_name='Figure1C')
 plot_dict = {'No_Label':df['Label-_DNA+'].dropna(), 
              'Label':df['Label+_DNA-'].dropna(),
              'DNA':df['Label+_DNA+'].dropna()}
+
+print(mannwhitneyu(plot_dict['DNA'], plot_dict['Label']))
+print(mannwhitneyu(plot_dict['DNA'], plot_dict['No_Label']))
 
 plt.figure(figsize=(2.9,3))
 plt.boxplot(plot_dict.values(), widths=0.5, showfliers=False)
@@ -85,8 +105,6 @@ plt.savefig('Figure1C.svg', dpi=300, facecolor='w', edgecolor='b',
         transparent=False, bbox_inches='tight', pad_inches=.05, metadata=None)
 plt.show()
 
-print(mannwhitneyu(plot_dict['DNA'], plot_dict['Label']))
-print(mannwhitneyu(plot_dict['DNA'], plot_dict['No_Label']))
 #%%
 """
 Figure 1D
@@ -97,6 +115,9 @@ plot_dict = {'DNA 1 d':df['DNA+_24h'].dropna(),
              'DNA 2 d':df['DNA+_48h'].dropna(),
              'Blank 1 d':df['DNA-_24h'].dropna(),
              'Blank 2 d':df['DNA-_48h'].dropna()}
+
+print(mannwhitneyu(plot_dict['DNA 1 d'], plot_dict['Blank 1 d']))
+print(mannwhitneyu(plot_dict['DNA 2 d'], plot_dict['Blank 2 d']))
 
 plt.figure(figsize=(3,3))
 plt.boxplot(plot_dict.values(), widths=0.5, showfliers=False)
@@ -119,10 +140,6 @@ plt.savefig('Figure1D.svg', dpi=300, facecolor='w', edgecolor='b',
         transparent=False, bbox_inches='tight', pad_inches=.05, metadata=None)
 plt.show()
 
-print(mannwhitneyu(plot_dict['DNA 1 d'], plot_dict['Blank 1 d']))
-print(mannwhitneyu(plot_dict['DNA 2 d'], plot_dict['Blank 2 d']))
-
-
 #%%
 """
 Figure 1F
@@ -132,15 +149,15 @@ df = pd.read_excel('SI_Data.xlsx', sheet_name='Figure1F')
 plot_dict = {'DNA':df['DNA+_48h'].dropna(), 
              'Blank':df['DNA-_48h'].dropna()}
 
+print(mannwhitneyu(plot_dict['DNA'], plot_dict['Blank']))
+
+plot_dict = transform(plot_dict)
 plt.figure(figsize=(1.6,3))
 plt.boxplot(plot_dict.values(),widths=0.5, showfliers=False)
 plt.xticks(ticks=[1,2], labels=plot_dict.keys(), fontsize=12, rotation=45)
 
 plt.scatter(simple_beeswarm2(plot_dict['DNA'])+1, plot_dict['DNA'], alpha=0.5, color='red', s=75)
 plt.scatter(simple_beeswarm2(plot_dict['Blank'])+2, plot_dict['Blank'], alpha=0.5, color='black', s=75, marker='D')
-
-#maximal biovolume of reporter strain (for both pilU and WT) at 4 d is ~10^5, -> limit of detection is set to 10^-5
-#to put the data on a log scale 0 values for the GFP channel, the transformaiton reporter channel, were set to 1x10^-5
 plt.hlines(y=10**-5, xmin=0.5, xmax=2.5, linewidth=1, linestyles='--', color='r')
 
 plt.yticks(np.arange(0,0.1,0.02),fontsize=12)
@@ -152,18 +169,6 @@ plt.savefig('Figure1F.svg', dpi=300, facecolor='w', edgecolor='b',
         orientation='portrait', format='svg',
         transparent=False, bbox_inches='tight', pad_inches=.05, metadata=None)
 plt.show()
-
-#untransform the data (10**-5 becomes 0)
-for key in list(plot_dict.keys()):
-    holder = []
-    for i in plot_dict[key]:
-        if i == 10**-5:
-            holder.append(0)
-        else:
-            holder.append(i)
-    plot_dict[key] = holder
-
-print(mannwhitneyu(plot_dict['DNA'], plot_dict['Blank']))
 #%%
 """
 Figure 2A
@@ -173,16 +178,17 @@ df = pd.read_excel('SI_Data.xlsx', sheet_name='Figure2A')
 plot_dict = {'WT':df['WT'].dropna(), 
              'pilU':df['pilU'].dropna()}
 
+print(mannwhitneyu(plot_dict['WT'], plot_dict['pilU']))
+
 plt.figure(figsize=(2,3))
 plt.boxplot(plot_dict.values(), widths=0.5, showfliers=False)
 plt.xticks(ticks=[1,2], labels=plot_dict.keys(), fontsize=12)
 
-plt.scatter((simple_beeswarm2(plot_dict['WT'])+1), plot_dict['WT'], alpha=0.5, color='black', s=75)
+plt.scatter((simple_beeswarm2(plot_dict['WT'], nbins=1)+1), plot_dict['WT'], alpha=0.5, color='black', s=75)
 plt.scatter((simple_beeswarm2(plot_dict['pilU'])+2), plot_dict['pilU'], alpha=0.5, color='blue', s=75)
-
 plt.axhline(y=1/2500, color='r', linestyle='--', linewidth=1)
 
-plt.ylim(10**-5, 10**0)
+plt.ylim(10**-6, 10**0)
 plt.yscale('log')
 plt.ylabel(r'Transformation Efficiency', fontsize=12)
 plt.xlabel(r'Strain', fontsize = 12)
@@ -191,9 +197,6 @@ plt.savefig('Figure2A.svg', dpi=300, facecolor='w', edgecolor='b',
         orientation='portrait', format='svg',
         transparent=False, bbox_inches='tight', pad_inches=.05, metadata=None)
 plt.show()
-
-print(mannwhitneyu(plot_dict['WT'], plot_dict['pilU']))
-
 #%%
 """
 Figure 2B
@@ -203,6 +206,9 @@ df = pd.read_excel('SI_Data.xlsx', sheet_name='Figure2B')
 plot_dict = {'WT':df['WT'].dropna(), 
              'pilU':df['pilU'].dropna()}
 
+print(mannwhitneyu(plot_dict['WT'], plot_dict['pilU']))
+
+plot_dict = transform(plot_dict)
 plt.figure(figsize=(2,3))
 plt.boxplot(plot_dict.values(), widths=0.5, showfliers=False)
 plt.xticks(ticks=[1,2], labels=plot_dict.keys(), fontsize=12)
@@ -210,8 +216,6 @@ plt.xticks(ticks=[1,2], labels=plot_dict.keys(), fontsize=12)
 plt.scatter((simple_beeswarm2(plot_dict['WT']))+1, plot_dict['WT'], alpha=0.5, c='black', s=75)
 plt.scatter((simple_beeswarm2(plot_dict['pilU']))+2, plot_dict['pilU'], alpha=0.5, c='blue', s=75)
 
-#maximum biovolume of reporter strain (for both pilU and WT) at 4 d is ~10^5, -> limit of detection is set to 10^-5
-#to put the data on a log scale 0 values for the GFP channel, the transformaiton reporter channel, were set to 1x10^-5
 plt.hlines(y=10**-5, xmin=0.5, xmax=2.5, linewidth=1, linestyles='--', color='r')
 
 plt.xticks(rotation=45)
@@ -223,16 +227,3 @@ plt.savefig('Figure2B.svg', dpi=300, facecolor='w', edgecolor='b',
         orientation='portrait', format='svg',
         transparent=False, bbox_inches='tight', pad_inches=.05, metadata=None)
 plt.show()
-
-#untransform the data (10**-4 becomes 0)
-for key in list(plot_dict.keys()):
-    holder = []
-    for i in plot_dict[key]:
-        if i == 10**-5:
-            holder.append(0)
-        else:
-            holder.append(i)
-    plot_dict[key] = holder
-
-test_res = mannwhitneyu(plot_dict['WT'], plot_dict['pilU'])
-print(test_res)
